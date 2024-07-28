4a1c01a2fa1711c06500039c1f946297f0ce5b8b
Paras Jain <pparasjain41@gmail.com>
2024-07-29 00:15:01 +0530

    add url

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url";
 const app = express();
 
 
 // app.use(express.static('../dist'))
+// app.use(express.static(path.join(__dirname, 'dist')));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files from the 'dist' folder
 app.use(express.static(path.join(__dirname, 'dist')));
+
 
 
 // app.get("/", (req, res) => {
