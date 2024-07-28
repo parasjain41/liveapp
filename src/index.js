5c495aa4a6c46d7b8d81a03877adf96a75c839fb
Paras Jain <pparasjain41@gmail.com>
2024-07-29 00:29:24 +0530

    done

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Serve static files from the 'dist' folder
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 
 
